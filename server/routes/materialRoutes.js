243ef0f4854ac8f7ca71c08064ee915736eff6c7
Mustafa Abdulhammed <160076019+DIJA003@users.noreply.github.com>
2026-04-14 03:29:35 +0200

    fix

--- server/routes/materialRoutes.js
+++ server/routes/materialRoutes.js
@@ -1,36 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const materialController = require('../controllers/MaterialController');
-const uploadMaterial = require('../middleware/uploadMaterial');
-
+const materialController = require("../controllers/MaterialController");
+const uploadMaterial = require("../middleware/upload");
 
 // GET ALL MATERIALS
-router.get('/', materialController.getAll);
-
+router.get("/", materialController.getAll);
 
 // GET MATERIAL BY ID
-router.get('/:id', materialController.getById);
-
+router.get("/:id", materialController.getById);
 
 // CREATE MATERIAL (UPLOAD FILE)
-router.post(
-  '/',
-  uploadMaterial.single('file'),
-  materialController.create
-);
-
+router.post("/", uploadMaterial.single("file"), materialController.create);
 
 // UPDATE MATERIAL
-router.put(
-  '/:id',
-  uploadMaterial.single('file'),
-  materialController.update
-);
-
+router.put("/:id", uploadMaterial.single("file"), materialController.update);
 
 // DELETE MATERIAL
-router.delete('/:id', materialController.remove);
-
+router.delete("/:id", materialController.remove);
 
 module.exports = router;
