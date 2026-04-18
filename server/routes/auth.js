2978e39f41b690737d8e1ef51f79cb0f643c3df5
Mustafa Abdulhammed <160076019+DIJA003@users.noreply.github.com>
2026-04-18 21:40:11 +0200

    start from scratch

--- server/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/verify-email', authController.verifyEmailExistence);
-
-module.exports = router;
